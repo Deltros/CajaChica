784cd7d4a0b6d468da02aa42b1d5d05be848ab33
Rolando <rolando.diazv92@gmail.com>
2026-04-18 18:11:17 -0400

    Añadido el logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|icon-.*\\.png|manifest\\.webmanifest).*)"],
 };
